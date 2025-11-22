7accf518d81cc9e33cf6ccdcd892202cdda332b6
mr5jt4qjzj-oss <mr5jt4qjzj@privaterelay.appleid.com>
2025-11-22 17:14:02 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Quando clicar no Instagram
+document.querySelector(".instagram").addEventListener("click", () => {
+    console.log("Redirecionando para Instagram...");
+});
+
+// Quando clicar no TikTok
+document.querySelector(".tiktok").addEventListener("click", () => {
+    console.log("Redirecionando para TikTok...");
+});
